6993aea8a2fd1771bad13df639f6654d5f5e1974
Peter D. Jorgensen <pdjorgensen@gmail.com>
2022-02-27 13:51:54 -0500

    OD and cleanup

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = (api) => {
 }
 
 class GarageDoorOpener {
-  constructor (log, config, api) {
+  constructor(log, config, api) {
     this.log = log
     this.config = config
     this.api = api
@@ -22,17 +22,17 @@ class GarageDoorOpener {
     this.targetDoorState = this.api.hap.Characteristic.TargetDoorState.CLOSED
 
     // Dump configuration
-    this.log("Configuration: " + JSON.stringify(this.config));
+    this.log.debug("Configuration: " + JSON.stringify(this.config, null, 2));
     this.GarageDoorController = new GarageDoorClass.GarageDoorController(log, this.config);
 
     this.GarageDoorService = new Service.GarageDoorOpener(this.name);
   }
 
-  getServices () {
-    const informationService = new Service.AccessoryInformation()
-        .setCharacteristic(Characteristic.Manufacturer, 'PJorg')
-        .setCharacteristic(Characteristic.Model, 'GarageDoorOpenerV2')
-        .setCharacteristic(Characteristic.SerialNumber, '002')
+  getServices() {
+    const InformationService = new Service.AccessoryInformation()
+      .setCharacteristic(Characteristic.Manufacturer, 'PJorg')
+      .setCharacteristic(Characteristic.Model, 'GarageDoorOpenerV2')
+      .setCharacteristic(Characteristic.SerialNumber, '002')
 
     this.GarageDoorService.getCharacteristic(Characteristic.CurrentDoorState)
       .on('get', this.getCurrentDoorStateCharacteristicHandler.bind(this))
@@ -44,38 +44,18 @@ class GarageDoorOpener {
     this.GarageDoorService.getCharacteristic(Characteristic.ObstructionDetected)
       .on('get', this.getObstructionDetectedCharacteristicHandler.bind(this))
 
-    return [informationService, this.GarageDoorService]
+    return [InformationService, this.GarageDoorService]
   }
 
-// We'll never be setting CurrentDoorState because it's supposed to be a representation of physical truth
-//  setCurrentDoorStateCharacteristicHandler (value, callback) {
-    /* this is called when HomeKit wants to update the value of the characteristic as defined in our getServices() function */
 
-    /*
-     * The desired value is available in the `value` argument.
-     * This is just an example so we will just assign the value to a variable which we can retrieve in our get handler
-     * In actuality, we'll need to pull this information from our door controller class eventually
-     */
-//    this.CurrentDoorState = value
-
-    /* Log to the console the value whenever this function is called */
-//    this.log('calling setCurrentDoorStateCharacteristicHandler', value)
-
-    /*
-     * The callback function should be called to return the value
-     * The first argument in the function should be null unless and error occured
-     */
-//    callback(null)
-//  }
-
-  getCurrentDoorStateCharacteristicHandler (callback) {
+  getCurrentDoorStateCharacteristicHandler(callback) {
     /*
      * this is called when HomeKit wants to retrieve the current state of the characteristic as defined in our getServices() function
      * it's called each time you open the Home app or when you open control center
      */
     this.log.debug('CurrentDoorState requested (getCurrentDoorStateCharacteristicHandler)...');
     const state_from_controller = this.GarageDoorController.checkDoorStatus();
-    if(typeof state_from_controller != 'undefined') {
+    if (typeof state_from_controller != 'undefined') {
       this.log.debug('Received state from controller... \n' + JSON.stringify(state_from_controller, null, 2));
       callback(null, state_from_controller.state.HomeKitState);
     } else {
@@ -84,81 +64,36 @@ class GarageDoorOpener {
   }
 
 
-
-  setTargetDoorStateCharacteristicHandler (value, callback) {
+  setTargetDoorStateCharacteristicHandler(value, callback) {
     /* this is called when HomeKit wants to update the value of the characteristic as defined in our getServices() function */
-
-    /*
-     * The desired value is available in the `value` argument.
-     * This is just an example so we will just assign the value to a variable which we can retrieve in our get handler
-     */
+    this.log(`TargetDoorState being set (setTargetDoorStateCharacteristicHandler):`, value);
     this.targetDoorState = value;
-
-    /* Log to the console the value whenever this function is called */
-    this.log(`calling setTargetDoorStateCharacteristicHandler`, value);
-
-    /*
-     * The callback function should be called to return the value
-     * The first argument in the function should be null unless and error occured
-     */
     callback(null);
   }
 
-  getTargetDoorStateCharacteristicHandler (callback) {
+
+  getTargetDoorStateCharacteristicHandler(callback) {
     /*
      * this is called when HomeKit wants to retrieve the current state of the characteristic as defined in our getServices() function
      * it's called each time you open the Home app or when you open control center
      */
-     this.log.debug('TargetDoorState requested (getTargetDoorStateCharacteristicHandler)...');
-     this.log.debug('TargetDoorState stored in Accessory class is: ', this.targetDoorState);
+    this.log.debug('TargetDoorState requested (getTargetDoorStateCharacteristicHandler)...');
+    this.log.debug('TargetDoorState stored in Accessory class is: ', this.targetDoorState);
     callback(null, this.targetDoorState)
   }
 
 
-
-
-
-
-
-  // We'll never be setting ObstructionDetected because it's supposed to be a representation of physical truth
-
-//  setObstructionDetectedCharacteristicHandler (value, callback) {
-    /* this is called when HomeKit wants to update the value of the characteristic as defined in our getServices() function */
-
-    /*
-     * The desired value is available in the `value` argument.
-     * This is just an example so we will just assign the value to a variable which we can retrieve in our get handler
-     */
-  //  this.isOn = value
-
-    /* Log to the console the value whenever this function is called */
-//    this.log(`calling setOnCharacteristicHandler`, value)
-
-    /*
-     * The callback function should be called to return the value
-     * The first argument in the function should be null unless and error occured
-     */
-  //  callback(null)
-//  }
-
-  getObstructionDetectedCharacteristicHandler (callback) {
+  getObstructionDetectedCharacteristicHandler(callback) {
     /*
      * this is called when HomeKit wants to retrieve the current state of the characteristic as defined in our getServices() function
      * it's called each time you open the Home app or when you open control center
      */
-
-    const od = 0;
-
-    /* Log to the console the value whenever this function is called */
-    this.log(`calling getObstructionDetectedCharacteristicHandler`, od)
-
-    /*
-     * The callback function should be called to return the value
-     * The first argument in the function should be null unless and error occured
-     * The second argument in the function should be the current value of the characteristic
-     * This is just an example so we will return the value from `this.isOn` which is where we stored the value in the set handler
-     */
-    callback(null, od)
+    this.log.debug('ObstructionDetected requested (getObstructionDetectedCharacteristicHandler)...');
+    const obstruction_state_from_controller = this.GarageDoorController.Obstruction;
+    if (typeof obstruction_state_from_controller != 'undefined') {
+      this.log.debug('Received obstruction state from controller... \n' + JSON.stringify(obstruction_state_from_controller, null, 2));
+      callback(null, obstruction_state_from_controller);
+    } else {
+      callback('Did not receive state data from controller class');
+    }
   }
-
-}
